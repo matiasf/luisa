4bcbf7c53818ac72fbc93be551bf0065eabf92ba
Matías Fernández <recta.de.euler@gmail.com>
2020-11-08 12:37:00 -0300

    Template base para sitio nuevo.

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div className="App">
 
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            {/*<nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
                 <div className="container">
                     <a className="navbar-brand" href="https://cruzar.uy/">Sistematización, cruzamiento y análisis de la
                         información</a>
@@ -258,50 +258,134 @@ function App() {
                                 </div>
                             </div>
                         </div>
-                        {/*<script>*/}
-                        {/*  function addScroll() {*/}
-                        {/*  // agrega scroll si el bloque es muy grande.*/}
-                        {/*  var imgs = $(".captcha-block");*/}
-                        {/*  var captchaBlock = $(".captcha-out");*/}
-                        {/*  for (i = 0; i < imgs.length; i++) {*/}
-                        {/*  var auxE = $(imgs[i]);*/}
-                        {/*  if (auxE.width() > captchaBlock.width()) {*/}
-                        {/*  auxE.css("overflow-x", "scroll");*/}
-                        {/*} else {*/}
-                        {/*  auxE.css("overflow-x", "visible");*/}
-                        {/*}*/}
-                        {/*}*/}
-                        {/*}*/}
-                        {/*  function toggleCollapse(jqElem){*/}
-                        {/*  if (jqElem.hasClass("show")) {*/}
-                        {/*  jqElem.removeClass("show");*/}
-                        {/*} else {*/}
-                        {/*  jqElem.addClass("show");*/}
-                        {/*}*/}
-                        {/*}*/}
-                        {/*  addScroll(); // Agrega scroll si el cuadro es muy grande*/}
-                        {/*  $("#hasCookies").val(navigator.cookieEnabled);*/}
+                        <script>
+                          function addScroll() {
+                          // agrega scroll si el bloque es muy grande.
+                          var imgs = $(".captcha-block");
+                          var captchaBlock = $(".captcha-out");
+                          for (i = 0; i < imgs.length; i++) {
+                          var auxE = $(imgs[i]);
+                          if (auxE.width() > captchaBlock.width()) {
+                          auxE.css("overflow-x", "scroll");
+                        } else {
+                          auxE.css("overflow-x", "visible");
+                        }
+                        }
+                        }
+                          function toggleCollapse(jqElem){
+                          if (jqElem.hasClass("show")) {
+                          jqElem.removeClass("show");
+                        } else {
+                          jqElem.addClass("show");
+                        }
+                        }
+                          addScroll(); // Agrega scroll si el cuadro es muy grande
+                          $("#hasCookies").val(navigator.cookieEnabled);
 
 
-                        {/*  // Despliega las instrucciones en hover.*/}
-                        {/*  $("#collIns").mouseenter(function(e){*/}
-                        {/*  $("#instrucciones").addClass("show");*/}
-                        {/*});*/}
-                        {/*  $("#commIns").mouseenter(function (e) {*/}
-                        {/*  $("#insComandos").addClass("show");*/}
-                        {/*});*/}
-                        {/*  $(".captcha").keydown(function(e){*/}
-                        {/*  if (e.which === 13 && !e.shiftKey) {*/}
-                        {/*  $("#fCaptcha").submit();*/}
-                        {/*  e.preventDefault();*/}
-                        {/*}*/}
-                        {/*});*/}
-                        {/*</script>*/}
+                          // Despliega las instrucciones en hover.
+                          $("#collIns").mouseenter(function(e){
+                          $("#instrucciones").addClass("show");
+                        });
+                          $("#commIns").mouseenter(function (e) {
+                          $("#insComandos").addClass("show");
+                        });
+                          $(".captcha").keydown(function(e){
+                          if (e.which === 13 && !e.shiftKey) {
+                          $("#fCaptcha").submit();
+                          e.preventDefault();
+                        }
+                        });
+                        </script>
                     </div>
                 </div>
             </article>
 
-            <div className="luisa-spacer"/>
+            <div className="luisa-spacer"/>*/}
+
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+                <div className="container">
+                    <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <header className="bg-primary text-white">
+                <div className="container text-center">
+                    <h1>Welcome to Scrolling Nav</h1>
+                    <p className="lead">A landing page template freshly redesigned for Bootstrap 4</p>
+                </div>
+            </header>
+
+            <section id="about">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 mx-auto">
+                            <h2>About this page</h2>
+                            <p className="lead">This is a great place to talk about your webpage. This template is
+                                purposefully unstyled so you can use it as a boilerplate or starting point for you own
+                                landing page designs! This template features:</p>
+                            <ul>
+                                <li>Clickable nav links that smooth scroll to page sections</li>
+                                <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+                                <li>Bootstrap's scrollspy feature which highlights which section of the page you're on
+                                    in the navbar
+                                </li>
+                                <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="services" className="bg-light">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 mx-auto">
+                            <h2>Services we offer</h2>
+                            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio
+                                velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus
+                                consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="contact">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 mx-auto">
+                            <h2>Contact us</h2>
+                            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio
+                                fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil
+                                beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <footer className="py-5 bg-dark">
+                <div className="container">
+                    <p className="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+                </div>
+            </footer>
         </div>
     );
 }
