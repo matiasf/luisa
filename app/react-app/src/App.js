b8b33bed1f9df1bf4721d65065befccb5e4d9789
Matías Fernández <recta.de.euler@gmail.com>
2020-11-24 13:30:51 -0300

    Integro efecto de animacion de la navegacion, refactor de algun componente.

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -1,13 +1,49 @@
-import IntroductionVis from './components/info/IntroductionVis';
+import React from "react";
+
+import IntroductionVis from './components/info/intro/IntroductionVis';
 import WelcomeInfo from './components/info/WelcomeInfo';
 import Help from './components/info/Help';
 import Privacy from './components/info/Privacy';
 import AboutUs from './components/info/AboutUs';
 import Images from './components/images/Images';
 
-function App() {
-    return (
-        <div className="App">
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    initScrollingNav() {
+        window.$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+            if (window.location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+                window.location.hostname === this.hostname) {
+                let target = window.$(this.hash);
+                target = target.length ? target : window.$('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    window.$('html, body').animate({
+                        scrollTop: (target.offset().top - 56)
+                    }, 1000, "easeInOutExpo");
+                    return false;
+                }
+            }
+        });
+
+        window.$('.js-scroll-trigger').click(function () {
+            window.$('.navbar-collapse').collapse('hide');
+        });
+
+        window.$('body').scrollspy({
+            target: '#mainNav',
+            offset: 56
+        });
+    }
+
+    componentDidMount() {
+        this.initScrollingNav()
+    }
+
+    render() {
+        return <div className="App">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
                 <div className="container">
                     <a className="navbar-brand js-scroll-trigger" href="#page-top"><strong>LUISA</strong></a>
@@ -24,7 +60,7 @@ function App() {
                             <li className="nav-item">
                                 <a className="nav-link js-scroll-trigger" href="#welcomeinfo">Proyecto</a>
                             </li>
-							<li className="nav-item">
+                            <li className="nav-item">
                                 <a className="nav-link js-scroll-trigger" href="#help">Instrucciones</a>
                             </li>
                             <li className="nav-item">
@@ -42,18 +78,17 @@ function App() {
                 </div>
             </nav>
 
-	    <section className="bg-warning text-black parallax" id="page-top">
+            <section className="bg-warning text-black parallax" id="page-top">
                 <IntroductionVis/>
             </section>
-			
-			<section id="images">
+
+            <section id="images">
                 <Images/>
             </section>
 
             <section id="welcomeinfo" className="text-white parallax">
                 <WelcomeInfo/>
             </section>
-
 
 
             <section id="help" className="text-white parallax">
@@ -74,7 +109,7 @@ function App() {
                 </div>
             </footer>
         </div>
-    );
+    };
 }
 
 export default App;
--- app/react-app/src/components/info/WelcomeInfo.js
+++ app/react-app/src/components/info/WelcomeInfo.js
@@ -14,58 +14,68 @@ function WelcomeInfo() {
             <div className="row">
                 <div className="col-3">
 
-                    <svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="20%"
+                    <svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                         height="20%"
                          fill="currentColor">
                         <circle cx="50%" cy="50%" r="50%" fill="grey"/>
-						 <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="100%"
-                         fill="currentColor">
-							<use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#receipt"/>
-						</svg>
-					</svg>
+                        <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                             height="100%"
+                             fill="currentColor">
+                            <use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#receipt"/>
+                        </svg>
+                    </svg>
 
                     <h4 className="text-center">Recuperación de Archivos </h4>
                     <p className="lead"> Se recuperaron archivos de varias fuentes donde se almacena información sobre
                         la dictadura uruguaya incluyendo
                         procedimientos represivos, fichas policiales, informes, etc.</p>
                 </div>
-                <div class="col-3 border border-white">
-				<svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="20%"
+                <div className="col-3 border border-white">
+                    <svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                         height="20%"
                          fill="currentColor">
                         <circle cx="50%" cy="50%" r="50%" fill="grey"/>
-						 <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="100%"
-                         fill="currentColor">
-							<use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#pencil"/>
-						</svg>
-					</svg>
+                        <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                             height="100%"
+                             fill="currentColor">
+                            <use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                        </svg>
+                    </svg>
                     <h4 className="text-center">Transcripción de Imágenes</h4>
-                    <p className="lead">Pedimos que nos ayude transcribiendo fotos de documentos, de la cual le ofrecemos una porción que
+                    <p className="lead">Pedimos que nos ayude transcribiendo fotos de documentos, de la cual le
+                        ofrecemos una porción que
                         contiene texto, símbolos o números.
                     </p>
                 </div>
-                <div class="col-3">
-								<svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="20%"
+                <div className="col-3">
+                    <svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                         height="20%"
                          fill="currentColor">
                         <circle cx="50%" cy="50%" r="50%" fill="grey"/>
-						 <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="100%"
-                         fill="currentColor">
-							<use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#braces"/>
-						</svg>
-					</svg>
+                        <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                             height="100%"
+                             fill="currentColor">
+                            <use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#braces"/>
+                        </svg>
+                    </svg>
 
                     <h4 className="text-center">Aplicación de Técnicas de Cruzamiento</h4>
                     <p className="lead">Se utilizan
-                        técnicas de cruzamiento de información para procesar de manera automatica y eficiente los contenidos de los documentos digitalizados.
+                        técnicas de cruzamiento de información para procesar de manera automatica y eficiente los
+                        contenidos de los documentos digitalizados.
                     </p>
                 </div>
-                <div class="col-3">
-								<svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="20%"
+                <div className="col-3">
+                    <svg viewBox="125 0 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                         height="20%"
                          fill="currentColor">
                         <circle cx="50%" cy="50%" r="50%" fill="grey"/>
-						 <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" class="bi" width="100%" height="100%"
-                         fill="currentColor">
-							<use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#eyeglasses"/>
-						</svg>
-					</svg>
+                        <svg viewBox="0 -30 200 200" preserveAspectRatio="xMaxYMax meet" className="bi" width="100%"
+                             height="100%"
+                             fill="currentColor">
+                            <use width="100%" height="65%" href="bootstrap-icons/bootstrap-icons.svg#eyeglasses"/>
+                        </svg>
+                    </svg>
 
                     <h4 className="text-center">Análisis</h4>
                     <p className="lead">Los documentos se categorizan y analisan para mejor entender el funcionamiento
@@ -78,18 +88,20 @@ function WelcomeInfo() {
             </p>
 
             <p className="lead"><a className="text-white" href="mailto:cruzar@fic.edu.uy?subject=Luisa: ">
-			<svg viewBox="0 0 50 50" preserveAspectRatio="xMaxYMax meet" class="bi" width="4%" height="4%"
+                <svg viewBox="0 0 50 50" preserveAspectRatio="xMaxYMax meet" className="bi" width="4%" height="4%"
+                     fill="currentColor">
+                    <use width="100%" height="100%" href="bootstrap-icons/bootstrap-icons.svg#envelope"/>
+                </svg>
+            </a> |
+                <a className="text-white"
+                   href="https://www.instagram.com/proyecto.luisa/?hl=es-la">
+                    <svg viewBox="0 0 50 50" preserveAspectRatio="xMaxYMax meet" className="bi" width="4%" height="4%"
                          fill="currentColor">
-							<use width="100%" height="100%" href="bootstrap-icons/bootstrap-icons.svg#envelope"/>
-					</svg></a> | 
-<a className="text-white"
-                href="https://www.instagram.com/proyecto.luisa/?hl=es-la">
-						 <svg viewBox="0 0 50 50" preserveAspectRatio="xMaxYMax meet" class="bi" width="4%" height="4%"
-                         fill="currentColor">
-							<use width="100%" height="100%" href="bootstrap-icons/bootstrap-icons.svg#camera"/>
-					</svg></a>
+                        <use width="100%" height="100%" href="bootstrap-icons/bootstrap-icons.svg#camera"/>
+                    </svg>
+                </a>
             </p>
-            <a className="btn btn-light" href="#images">¡A las imágenes!</a>
+            <a className="btn btn-light js-scroll-trigger" href="#images">¡A las imágenes!</a>
         </div>
     );
 
