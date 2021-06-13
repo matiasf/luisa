4f7d0c7df5fed98043865afa094b2dc6cdbe74a7
unknown <cbisogno@terpmail.umd.edu>
2021-06-13 15:14:20 -0300

    traduccion ingles AboutUs

--- app/react-app/src/localized-content-component/about-us/AboutUs-en.js
+++ app/react-app/src/localized-content-component/about-us/AboutUs-en.js
@@ -2,26 +2,19 @@ function AboutUs() {
 
     return (
         <div className="container">
-            <h2>How we are</h2>
+            <h2>Who we are</h2>
 
-            <p className="lead"> Abocamos docentes-investigadores y estudiantes de la <u><a
+            <p className="lead"> We are professors, researchers, and students from the <u><a
                 className="text-white"
-                href="http://www.fiC.edu.uy">Facultad
-                de Información y Comunicación (FIC) </a></u>y de la <u><a className="text-white"
-                                                                          href="http://www.fing.edu.uy">Facultad de
-                Ingeniería (FIng)</a></u>, así como miembros de <u><a className="text-white" href="https://desaparecidos.org.uy/">Madres
-                y Familiares de Uruguayos Detenidos-Desaparecidos</a></u>.
-                Dichos grupos, a traves del desarrollo de estrategias y herramientas de procesiamiento de datos, buscamos divulgar
-                la informacion recabada.</p>
-            <p className="lead">En el marco del llamado 2017 a Proyectos de Extensión que emitió
-                la <u><a className="text-white"
-                         href="http://www.extension.udelar.edu.uy/">Comisión
-                    Sectorial de Extensión y Actividades con el Medio (CSEAM) </a></u>de la <u><a className="text-white"
-                                                                                                  href="http://www.universidad.edu.uy">Universidad
-                    de la República (UdelaR), </a></u>comenzó a funcionar este proyecto de sistematización,
-                tratamiento y difusión de la
-                información contenida en los documentos recuperados.</p>
-
+                href="http://www.fiC.edu.uy">College of Communication (FIC)</a></u>and the <u><a className="text-white"
+                                                                          href="http://www.fing.edu.uy">Engineering College</a></u>, as well as members of the organization of <u><a className="text-white" href="https://desaparecidos.org.uy/">Mothers
+                and Families of Detained-Disappeared Uruguayans</a></u>.
+                These groups collaborate to collect and process documents to make accesible the information collected to the public.</p>
+            <p className="lead">In 2017, the <u><a className="text-white"
+                                                                                                  href="http://www.universidad.edu.uy">University of the Republic's (UdelaR), </a></u>
+																								  <u><a className="text-white"
+                         href="http://www.extension.udelar.edu.uy/">Sectorial Commission on Extension and Activities in Media (CSEAM)</a></u> proposed an Extension Project
+						to collect, process, and publicize the information contained in the unearthed documents.</p>
             <br/>
             <br/>
 
@@ -33,7 +26,7 @@ function AboutUs() {
                             <button className="btn btn-link text-dark collapsed"
                                     data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false"
                                     aria-controls="collapseOne">
-                                Facultad de Información y Comunicación (FIC)
+                                College of Communication (FIC)
                             </button>
                         </h5>
 
@@ -41,14 +34,14 @@ function AboutUs() {
                     <div id="collapseOne" className="collapse show rgba-black-light white-text lead" aria-labelledby="headingOne"
                          data-parent="#accordion">
                         <div className="card-body parallax">
-                            <b>Sección Periodismo:</b>
+                            <b>Journalism Section:</b>
                             <ul>
-                                <li>Profesor Adjunto Samuel Blixen</li>
-                                <li>Asistente Betania Núñez</li>
+                                <li>Adjunct Professor Samuel Blixen</li>
+                                <li>Assistant Betania Núñez</li>
                             </ul>
-                            <b>Departamento de Fuentes Documentales, Recursos y Servicios:</b>
+                            <b>Source Documents, Resources and Services Departament:</b>
                             <ul>
-                                <li>Profesor Adjunto Fabián Hernández Muñiz</li>
+                                <li>Adjunct Professor Fabián Hernández Muñiz</li>
                             </ul>
                         </div>
                     </div>
@@ -59,22 +52,22 @@ function AboutUs() {
                             <button className="btn btn-link  text-dark collapsed"
                                     data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
                                     aria-controls="collapseTwo">
-                                Facultad de Ingeniería (FIng)
+                                Engineering College (FIng)
                             </button>
                         </h5>
                     </div>
                     <div id="collapseTwo" className="collapse lead" aria-labelledby="headingTwo" data-parent="#accordion">
                         <div className="card-body parallax">
-                            <b>Instituto de Computación:</b>
+                            <b>Computer Science Institute:</b>
                             <ul>
-                                <li>Profesor Emérito Juan José Cabezas</li>
-                                <li>Profesor Adjunto Fernando Carpani</li>
-                                <li>Profesora Adjunta Lorena Etcheverry</li>
+                                <li>Professor Emeritus Juan José Cabezas</li>
+                                <li>Adjunct Profesosr Fernando Carpani</li>
+                                <li>Adjunct Professor Lorena Etcheverry</li>
                             </ul>
-                            <b>Instituto de Ingeniería Eléctrica:</b>
+                            <b>Electrical Engineering Institute:</b>
                             <ul>
-                                <li>Profesor Titular Gregory Randall</li>
-                                <li>Profesor Adjunto Ignacio Ramírez (Coordinador del equipo)</li>
+                                <li>Titular Professor Gregory Randall</li>
+                                <li>Adjunct Professor Ignacio Ramírez (Team Leader)</li>
                             </ul>
                         </div>
                     </div>
@@ -90,21 +83,17 @@ function AboutUs() {
                     </div>
                     <div id="collapseThree" className="collapse lead" aria-labelledby="headingThree" data-parent="#accordion">
                         <div className="card-body parallax">
-                            <p><b>Madres y Familiares de Uruguayos Detenidos-Desaparecidos</b></p>
+                            <p><b>Mothers and Families of Detained-Disappeared Uruguayans</b></p>
                             <p>Un grupo de madres y familiares de uruguayos detenidos desaparecidos que
                                 desde los años ´70 empezaron a juntarse para buscarlos, aún con la esperanza de
                                 encontrarlos vivos. Desde la apertura democrática caminan juntos respetando
                                 la diversidad de pensamientos que los caracteriza pero unidos en torno a sus principales
                                 objetivos: Verdad, justicia, memoria y nunca más.</p>
-                            <p><b>Grupo de Trabajo por Verdad y Justicia</b></p>
-                            <p>Desde el momento de su creación el 19 de
-                                mayo de 2015 el Grupo de Trabajo por Verdad y Justicia investiga los crímenes de lesa humanidad
-                                cometidos por agentes del Estado o contando con su complicidad, durante el
-                                período de actuación ilegal o en su fase terrorista.
-                                El Grupo se constituyó para contribuir en dar luz a tan graves hechos en
-                                pos de la verdad histórica y promover la justicia en el marco del Estado de
-                                Derecho sobre la base de estándares internacionales de Verdad, Justicia,
-                                Memoria y Garantías de No Repetición.
+                            <p><b>Working Group for Truth and Justice</b></p>
+                            <p>From its founding on May 19, 2015 the Working Group for Truth and Justice has been dedicated to the investigation 
+								of human rights abuses in which State agents activley participated, or were complicit. The Working Group's objective 
+								is to shed light on these illegal acts of State terrorism and promote international standards of Truth, Justice,
+								Memory, and the Guarantee of Non Repetition.
                             </p>
                         </div>
                     </div>
@@ -114,24 +103,20 @@ function AboutUs() {
                         <h5 className="mb-0">
                             <button className="btn btn-link  text-dark collapsed" data-bs-toggle="collapse"
                                     data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                Recursos de Apoyo
+                                Supporting Resources
                             </button>
                         </h5>
                     </div>
                     <div id="collapseFour" className="collapse lead" aria-labelledby="headingFour" data-parent="#accordion">
                         <div className="card-body parallax">
-                            <p>Las tres millones de imágenes con las que cuenta el Proyecto LUISA provienen de una variedad de
-                                fuentes incluyendo
-                                los documentos del Cuerpo de Fusileros Navales y el Archivo Histórico de la ex Dirección Nacional
-                                de Información e
-                                Inteligencia.
-                                También se suma a este proceso aquella documentación a la que se acceda a instancias del
-                                Grupo de Trabajo por Verdad y Justicia y en el marco de las acciones de investigación e
-                                intervención en los distintos archivos públicos.</p>
-                            <p><b>Archivo Berrutti</b></p>
-                            <p> Miles de archivos microfilmados que fueron encontrados en el Ministerio de Defensa, en la casa del
-                                coronel Castiglioni, y en la ex Escuela de Inteligencia del Ejército. Se recolectaron durante la
-                                gestión de la ministra Azucena Berrutti.
+                            <p>The LUISA Project relies on three million images from several sources including documents from the Naval Rifle Corps
+								and the National Office of Information and Intelligence's Historical Archive.
+                                The Project also has access to documentation recovered by the Working Group for Truth and Justice as a result of their                               Grupo de Trabajo por Verdad y Justicia y en el marco de las acciones de investigación e
+                                interventions in several public archives.</p>
+                            <p><b>Berrutti Archive</b></p>
+                            <p> Thousands of microfilmed files were found in the Department of Defense, in the home of 
+                                colonel Castiglioni, and in the Military's former Inteligence School. These documents were retrieved
+								during the leadership of Secretary Azucena Berrutti.
                             </p>
                         </div>
                     </div>
