73fbe330d207e208d43240fe785c9c162bfcb38c
unknown <cbisogno@terpmail.umd.edu>
2021-06-13 22:21:44 -0300

    privacidad traducida ingles

--- app/react-app/src/localized-content-component/privacy/Privacy-en.js
+++ app/react-app/src/localized-content-component/privacy/Privacy-en.js
@@ -2,18 +2,16 @@ function Privacy() {
 
     return (
         <div className="container">
-            <h2>Privacy agreement</h2>
+            <h2>Privacy Agreement</h2>
             <p className="lead">
-                Este sistema se encuentra en fase de prueba.
-                Durante esta fase utilizaremos cookies para distinguir las conexiones al sistema, pero no recabaremos
-                ningún
-                dato que permita reidentificar al dispositivo desde el cual se ingresó al sistema, asi como ningún dato
-                sobre el dispositivo o datos personal del usuario.
-                Esta información será empleada únicamente para análisis estadístico y para mejorar el desempeño del
-                sistema.
-                Usted puede eliminar las cookies en cualquier momento desde su dispositivo, le rogamos que no lo haga
-                durante la fase de prueba.
-                Una vez completada la misma se darán indicaciones adecuadas para eliminarlas.
+                This sistem is currently in a testing phase.
+                During this phase the system will be using cookies to distinguish unique connections to the system, 
+				but no personal or identifying data will be stored from the device used to access the system.
+                This information will be used solely for statistical analysis and to improve the functionality 
+				of the system.
+                Users can eliminate cookies at any time from their devices. We ask that users refrain from doing so 
+				during the testing phase.
+                Once this phase is complete, further instruction will be provided on how to eliminate cookies.
             </p>
         </div>
     );
